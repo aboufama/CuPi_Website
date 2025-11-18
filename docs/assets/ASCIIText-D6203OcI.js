8e5b69b7f92e3d28c156b5b7e67b68eaad074117
Andre Boufama <andreboufama@dhcp-vl2042-23154.redrover.cornell.edu>
2025-11-18 17:24:34 -0500

    settings

--- docs/assets/ASCIIText-D6203OcI.js
+++ docs/assets/ASCIIText-D6203OcI.js
@@ -1,4 +1,4 @@
-import{r as vi,j as lo}from"./index-CInF2T8X.js";/**
+import{r as vi,j as lo}from"./index-3uLyNCX8.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT

0aac79f1e30e0c603d91687a8aa11bcd82c2a81d
Andre Boufama <andreboufama@dhcp-vl2042-23154.redrover.cornell.edu>
2025-11-18 14:52:28 -0500

    Fix base path and favicon for GitHub Pages deployment

--- docs/assets/ASCIIText-DQhF9Ek3.js
+++ docs/assets/ASCIIText-DQhF9Ek3.js
@@ -1,4 +1,4 @@
-import{r as vi,j as lo}from"./index-DgBun_Lq.js";/**
+import{r as vi,j as lo}from"./index-DmtTPhUc.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
